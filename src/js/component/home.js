d4f5f73d14639eb8ca4f9e185b072014452bf64e
Gerdeth <gerdethwickham@gmail.com>
2020-07-29 19:02:13 +0000

    colors added

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faClock,
+	faCloudShowersHeavy
+} from "@fortawesome/free-solid-svg-icons";
+import "../../styles/index.scss";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+let colors = ["purple", "blue", "orange", "yellow", "red", "green", "orange"];
+
 export function Home(props) {
+	function ranColor() {
+		let index = Math.floor(Math.random() * colors.length);
+		let colOutput = colors[index];
+
+		return colOutput;
+	}
 	return (
-		<div className="text-center mt-5">
-			<div>{props.char1} </div>
-			<div>{props.char2}</div>
-			<div>{props.char3}</div>
-			<div>{props.char4}</div>
-			<div>{props.char5}</div>
-			<div>{props.char6}</div>
+		<div className="text-center mt-5 d-flex clock-container }">
+			<div style={{ background: ranColor() }}>
+				<FontAwesomeIcon icon={faClock} />
+			</div>
+			<div style={{ background: ranColor() }}>{props.char1}</div>
+			<div style={{ background: ranColor() }}>{props.char2}</div>
+			<div style={{ background: ranColor() }}>{props.char3}</div>
+			<div style={{ background: ranColor() }}>{props.char4}</div>
+			<div style={{ background: ranColor() }}>{props.char5}</div>
+			<div style={{ background: ranColor() }}>{props.char6}</div>
 		</div>
 	);
 }
